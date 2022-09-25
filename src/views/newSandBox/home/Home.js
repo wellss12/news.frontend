c3bb589d6785dddbbb64505c840c6f5bd7f530b0
Wells <ll0918900@gmail.com>
2022-09-25 18:39:25 +0800

    [feat] use json-server mock data

--- src/views/newSandBox/home/Home.js
+++ src/views/newSandBox/home/Home.js
@@ -1,9 +1,22 @@
-﻿import React from "react";
+﻿import React, {useEffect, useState} from "react";
 import {Button} from "antd";
+import axios from 'axios';
+
+const ajax = () => {
+    axios.get("http://localhost:8000/posts").then(res => {
+        console.log(res.data)
+    })
+    axios.post("http://localhost:8000/posts", {
+        "title": "ugly",
+        "author": "DDD"
+    });
+}
 
 export function Home() {
+    const [mockData, setMockData] = useState([]);
+
     return <div>
         Home
-        <Button type="primary">Button</Button>
+        <Button type="primary" onClick={ajax}>Button</Button>
     </div>
 }
