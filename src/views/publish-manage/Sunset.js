a0d63d78e946692da9dc97e9fe8e4ceeb487b999
Wells <ll0918900@gmail.com>
2022-11-26 11:13:22 +0800

    [feat] Implement Sunset view

--- src/views/publish-manage/Sunset.js
+++ src/views/publish-manage/Sunset.js
@@ -1,9 +1,13 @@
 ﻿import React from 'react';
+import useNews from "../../components/publish-manage/useNews";
+import NewsPublish from "../../components/publish-manage/NewsPublish";
 
 export default function Sunset(props) {
+    const {newsList} = useNews(3);
+    
     return (
         <div>
-            Sunset
+            <NewsPublish newsList={newsList}/>
         </div>
     );
 }
